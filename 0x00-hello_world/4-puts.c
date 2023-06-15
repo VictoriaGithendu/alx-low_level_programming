f3385010a20871e88dabf43d7280d03d9622ed8a
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-15 12:52:56 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include<stdio.h>
-
 /**
- * main - Entry point
- *
+ * main - Puts function program
  * Return: If 0 (Success)
  */
-
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }	
