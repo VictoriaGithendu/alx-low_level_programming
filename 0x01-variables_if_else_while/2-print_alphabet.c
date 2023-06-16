13f34da0cf647944be79ec49da0091d32efc4273
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-16 14:11:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Prints alphabet in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+putchar('\n');
+}
+return (0);
+}
