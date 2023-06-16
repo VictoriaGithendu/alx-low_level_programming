ddac216081ef42bc93a9a7fc13e34a3c51fabab9
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-16 14:26:41 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,12 @@ int main(void)
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-putchar("ch\n");
+putchar(ch);
 }
 for (ch = 'A'; ch <= 'Z'; ch++)
 {
-putchar("ch\n");
+putchar(ch);
+putchar('\n');
 }
 return (0);
 }
