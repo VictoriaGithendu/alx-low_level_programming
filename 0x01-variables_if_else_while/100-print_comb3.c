919e8957eff76177e01e5a530306100d0e1eae21
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-17 00:27:48 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - Prints different combinations of two digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int x, y;
+for (x = 18; x <= 26; x++)
+{
+	for (y = 19; y <= 27; y++)
+	{
+		if (y > x)
+			putchar(x);
+		putchar(y);
+		if (y != 26 && x != 27)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
