9471a7a8ceefa0afdb8abc51031268e1842188c1
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-19 10:53:11 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 				putchar((x % 10) + 48);
 				putchar((y / 10) + 48);
 				putchar((y % 10) + 48);
-				if (x != 98 || j != 99)
+				if (x != 98 || y != 99)
 				{
 					putchar(',');
 					putchar(' ');
