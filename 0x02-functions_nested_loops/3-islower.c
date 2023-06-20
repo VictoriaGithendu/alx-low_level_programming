4ca5c855ce6c3d6811998c9ae83a91ad0ab4ef05
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-20 11:33:46 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - Prints lowercase characters
+ * @c: The character to be checked
+ * Return: 1 for lowercase and 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 &&c <= 122)
+		return (1);
+	else
+		return (0);
+}
