7d01dbe00db5aba363d8bd6091c051ed040829c3
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-20 15:37:29 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,32 +7,35 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
-
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; n <= 98; n++)
 		{
-			if (i != 98)
+			if (n == 98)
 			{
-				printf("%d", i);
+				printf("%d", n);
+				printf("\n");
+				break;
 			}
-			else if (i == 98)
+			else
 			{
-				printf("%d\n", i);
+				printf("%d, ", n);
 			}
-			else if (n >= 98)
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
 			{
-				for (j = n; j >= 98; j--)
-				
-				if (j != 98)
-				{
-					printf("%d", j);
-				}
-				else if (j == 98)
-				{
-					printf("%d\n", j);
-				}
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
 			}
 		}
 	}
