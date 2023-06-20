17ad9bf324135660f5113eb3bf4adba7de136187
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-20 08:54:49 -0700

    2-print_alphabet_x10.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Lowercase alphabets
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int x = 0;
+
+	while (x < 0)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
