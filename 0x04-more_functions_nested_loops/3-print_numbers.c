e386bf72b9091f8d91478ecbfa0d2add5268cfca
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-22 05:56:24 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,12 @@
  */
 void print_numbers(void)
 {
-	int x;
+	char x;
 
-	if (x >= 0 && x <= 9)
+	for (x = 0; x <= 9; x++ )
 	{
 	_putchar(x);
-	_putchar('\n');
 	}
-	return (0);
+	_putchar('\n');
 }
 
