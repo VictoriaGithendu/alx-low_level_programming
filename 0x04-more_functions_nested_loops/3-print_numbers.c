6bf6e7767a7ccde74c71124aac38713d7720b55d
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-22 05:42:25 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ * Return: 0 (Success)
+ */
+void print_numbers(void)
+{
+	int x;
+
+	if (x >= 0 && x <= 9)
+	{
+	_putchar(x);
+	_putchar('\n');
+	}
+	return (0);
+}
+
