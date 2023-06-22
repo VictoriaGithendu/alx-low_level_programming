b76f88a5284053ec2bd72dc938b1bd8007969279
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-22 06:11:58 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints numbers apart from 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (x != 2 || x != 4)
+			_putchar(x);
+	}
+	_putchar('\n');
+}
+
