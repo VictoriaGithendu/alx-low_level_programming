5d2e1b12011e3eaa6721c1ee767387f23b90bff6
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-26 12:27:48 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (s[x])
+		x++;
+	while (x--)
+		_putchar(s[x]);
+	_putchar('\n');
+}
