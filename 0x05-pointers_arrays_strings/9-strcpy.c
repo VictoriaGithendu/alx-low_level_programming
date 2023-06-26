96cd95ff8acfa8bfebb88492d0ca1ca71e7c8223
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-26 13:45:13 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string pointed to by src
+ * @dest: char string type
+ * @src: char string type
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x = -1;
+
+	do {
+		x++;
+		dest[x] = src[x];
+	} while (src[x] != '\0');
+
+	return (dest);
+}
