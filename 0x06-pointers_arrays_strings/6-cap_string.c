faaee47284d565a5b4213cd1031a59859677f197
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-29 14:41:23 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,22 +12,16 @@ char *cap_string(char *x)
 
 	while (x[count] != '\0')
 	{
-		if (x[0] >= 97 && x[0] <= 122)
-		{
-			x[0] = x[0] - 32;
-		}
-		if (x[count] == ' ' || x[count] == '\t' || x[count] == '\n'
-				|| x[count] == ',' || x[count] == ';' || x[count] == '.' ||
-				x[count] == '!' || x[count] == '?' || x[count] == '"' ||
-				x[count] == '(' || x[count] == '(' || x[count] == ')' || x[count] == '{'
-				|| x[count] == '}')
-		{
-			if (x[count + 1] >= 97 && x[count + 1] <= 122)
-			{
-				x[count + 1] = x[count + 1] - 32;
-			}
+		while (x[count] >= 97 && x[count] <= 122)
 			count++;
-		}
+
+		if (x[count - 1] == ' ' || x[count - 1] == '\t' || x[count - 1] == '\n'
+				|| x[count - 1] == ',' || x[count - 1] == ';' || x[count - 1] == '.' ||
+				x[count - 1] == '!' || x[count - 1] == '?' || x[count - 1] == '"' ||
+				x[count - 1] == '(' || x[count - 1] == ')' ||
+				x[count - 1] == '{' || x[count - 1] == '}' || count == 0)
+			x[count] -= 32;
+		count++;
 	}
 	return (x);
 }
