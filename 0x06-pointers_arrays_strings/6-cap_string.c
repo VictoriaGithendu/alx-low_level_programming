f766538ba6ae3ccd9d24f08a048a812324e1254f
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-29 15:01:10 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,9 +10,9 @@ char *cap_string(char *x)
 
 	count = 0;
 
-	while (x[count] != '\0')
+	while (x[count])
 	{
-		while (x[count] >= 97 && x[count] <= 122)
+		while (!(x[count] >= 97 && x[count] <= 122))
 			count++;
 
 		if (x[count - 1] == ' ' || x[count - 1] == '\t' || x[count - 1] == '\n'
