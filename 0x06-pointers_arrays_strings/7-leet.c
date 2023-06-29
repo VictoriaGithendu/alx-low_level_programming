f44cf3ecf05a88e8ee7c5b5c724eecbed668059c
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-06-29 15:24:53 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *leet - encodes a string
+ * *leet - encodes a string into 1337
  * @x: input
  * Return: 0
  */
@@ -8,12 +8,12 @@ char *leet(char *x)
 {
 	int y, z;
 
-	char s1[] = "aAeEoOtTlL";
+	char s1[] = "aAeEoOtTIL";
 	char s2[] = "4433007711";
 
 	for (y = 0; x[y] != '\0'; y++)
 	{
-		for (z = 0; z != '\0'; z++)
+		for (z = 0; z < 10; z++)
 		{
 			if (x[y] == s1[z])
 			{
