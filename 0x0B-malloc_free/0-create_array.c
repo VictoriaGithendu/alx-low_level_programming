b03746ee356ad96e118d7d0b1388588d07bbd6cd
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-07-11 07:58:08 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,8 +12,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int x;
 
 	str = malloc(sizeof(char) * size);
+	{
 	if (size == 0 || str == NULL)
-
+		return (NULL);
+	}
 		for (x = 0; x < size; x++)
 			str[x] = c;
 	return (str);
