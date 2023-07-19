26c83071aeee041b55c05bf8a19f9c42f1c95b3c
VictoriaGithendu <victoriagithendu@gmailoo.com>
2023-07-19 11:09:25 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - prints opcodes
+ * @argc: no of arguements
+ * @argv: array of arguements
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	char *s;
+
+	int bytes, x;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	s = (char *)main;
+	for (x = 0; x < bytes; x++)
+	{
+		if (x == bytes -1)
+		{
+			printf("%02hhx\n", s[x]);
+			break;
+		}
+		printf("%02hhx\n", s[x]);
+	}
+	return (0);
+}
