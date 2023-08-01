4e935d5e3996b739fd96db6880785bf340507569
VictoriaGithendu <victoriagithendu@gmail.com>
2023-08-01 11:20:06 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,8 +14,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
-	n = new->n;
-	*head = new->next;
-	*head = new;
+	new->n = n;
+	new->next = *head;
+	new = *head;
 	return (new);
 }
