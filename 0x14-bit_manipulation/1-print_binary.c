f719a19cf5442ad2d0bc4ef6aa5e8895a3c02fb1
VictoriaGithendu <victoriagithendu@gmail.com>
2023-08-03 13:12:05 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,12 +15,12 @@ void print_binary(unsigned long int n)
 		result = n >> num;
 		if (result & 1)
 		{
-			_putchar ('1');
+			_putchar('1');
 			count++;
 		}
 		else if (count)
-			_putchar ('0);
+			_putchar('0);
 	}
 	if (!count)
-		_putchar ('0');
+		_putchar('0');
 }
