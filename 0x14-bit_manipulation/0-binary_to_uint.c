7416f2bd8add2c73b0f40f246827189265fe824b
VictoriaGithendu <victoriagithendu@gmail.com>
2023-08-03 12:58:27 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (num = 0; b[num]; num++)
 	{
-		if (b[num] < '0' || b[num] > 1)
+		if (b[num] < '0' || b[num] > '1')
 			return (0);
 		result = 2 * result + (b[num] - '0');
 	}
