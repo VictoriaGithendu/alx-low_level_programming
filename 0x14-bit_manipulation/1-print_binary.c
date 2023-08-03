1329de95958f6d3b517c08defaebe2fa7ae11b75
VictoriaGithendu <victoriagithendu@gmail.com>
2023-08-03 13:10:11 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_binary - prints binary
+ * @n: integer
+ * Return: binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int result;
+	int num, count = 0;
+
+	for (num = 63; num >= 0; num--)
+	{
+		result = n >> num;
+		if (result & 1)
+		{
+			_putchar ('1');
+			count++;
+		}
+		else if (count)
+			_putchar ('0);
+	}
+	if (!count)
+		_putchar ('0');
+}
