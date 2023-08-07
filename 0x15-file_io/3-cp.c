f8b2e283cd6e99ffee3f7919b06daaa158ef1333
VictoriaGithendu <victoriagithendu@gmail.com>
2023-08-07 14:36:23 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,38 +2,6 @@
 #include<stdlib.h>
 #include<stdio.h>
 /**
- * create_buffer - function to allocate 1024 bytes to buffer
- * @file: file buffer name
- * Return: pointer to buffer
- */
-char *create_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		exit(99);
-	}
-	return (buffer);
-}
-/**
- * close_file - Function to close file descriptors
- * @fd: Closed file descriptor
- */
-void close_file(int fd)
-{
-	int x;
-
-	x = close(fd);
-	if (x == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-/**
  * main - Function to copy file contents
  * @argc: no of arguements
  * @argv: array of arguements
@@ -49,29 +17,69 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = create_buffer(argv[2]);
-	x = open(argv[1], O_RDONLY);
-	z = read(x, buffer, 1024);
-	y = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = malloc(sizeof(char) * 1024);
+	if (!buffer)
+		return (0);
+	y = open(argv[1], O_RDONLY);
+	check_98(y, buffer, argv[1]);
+	x = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	check_99(x, buffer, argv[2]);
 	do {
-		if (x == -1 || z == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
-		v = write(y, buffer, z);
-		if (y == -1 || v == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
-		z = read(x, buffer, 1024);
-		y = open(argv[2], O_WRONLY, O_APPEND);
-	} while (z > 0);
+		z = read(y, buffer, O_RDONLY);
+		if (z == 0)
+			break;
+		check_98(z, buffer, argv[1]);
+		v = write(x, buffer, z);
+		check_99(v, buffer, argv[2]);
+	} while (v >= 1024);
+	z = close(x);
+	check_100(z, buffer);
+	z = close(y);
+	check_100(z, buffer);
 	free(buffer);
-	close_file(x);
-	close_file(y);
 	return (0);
+}
+/**
+ * check_98  - checks error 98
+ * @buffer: Buffer
+ * @argv: array of arguements
+ * @x: Value
+ */
+void check_98(int x, char *argv[], char *buffer)
+{
+	if (x < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
+		exit(98);
+	}
+}
+/**
+ * check_99 - Checks error 99
+ * @buffer: Buffer
+ * @argv: array of arguements
+ * @x: value
+ */
+void check_99(int x, char *buffer, char *argv[])
+{
+	if (x < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
+		free(buffer);
+		exit(99);
+	}
+}
+/**
+ * check_100 - Checks error 100
+ * @x: value
+ * @buffer: Buffer
+ */
+void check_100(int x, char *buffer)
+{
+	if (x < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", x);
+		free(buffer);
+		exit(100);
+	}
 }
