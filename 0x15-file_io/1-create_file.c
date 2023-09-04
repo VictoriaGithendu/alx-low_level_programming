136c506e09d6eb7527708dfba84760ac75cbd10f
VictoriaGithendu <victoriagithendu@gmail.com>
2023-09-04 11:59:22 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,21 +7,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, length;
+	int fd, w, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length])
-		       	length++;
+		for (length = 0; text_content[length]; length++)
+			;
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, length);
-
-	if (fd == -1 || w = -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	{
 		return (-1);
-
+	}
+	if (length > 0)
+	{
+		w = write(fd, text_content, length);
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 	close(fd);
 	return (1);
 }
