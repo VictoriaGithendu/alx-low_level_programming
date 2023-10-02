27d3189a35edd45f03010712e279ef1f27cbc79a
VictoriaGithendu <victoriagithendu@gmail.com>
2023-10-02 04:31:34 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -48,27 +48,33 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = create_buffer(argv[1]);
+	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_file(from);
+		exit(99);
+	}
 	do {
-		if (from == -1 || r == -1)
+		r = read(from, buffer, 1024);
+		if (r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
 			exit(98);
 		}
 		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		if (w == -1)
 		{
 			dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
-			free(buffer);
 			exit(99);
 		}
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
 	free(buffer);
 	close_file(from);
